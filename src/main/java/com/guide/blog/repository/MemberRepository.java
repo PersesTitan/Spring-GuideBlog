1f3d5d4ac201c2dcc7eefcdd7c0d94e8c80bc911
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-02 00:22:20 +0900

    Repository 수정

--- src/main/java/com/guide/blog/repository/MemberRepository.java
+++ src/main/java/com/guide/blog/repository/MemberRepository.java
@@ -1,12 +1,33 @@
 package com.guide.blog.repository;
 
 import com.guide.blog.domain.Member;
+import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(long id);
+@Repository
+public class MemberRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * 저장
+     * @param member 정보 가져옴
+     * @return 아이디 반환
+     */
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    /**
+     * 아이디 조회
+     * @param id 아이디 받아오기
+     * @return 조회된 아이디 반환
+     */
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
