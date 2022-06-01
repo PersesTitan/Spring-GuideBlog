c3e71e4763777b19ce9d5d9cb8645b18e5984b59
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-01 15:23:10 +0900

    Initial commit

---
+++ src/test/java/com/guide/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
