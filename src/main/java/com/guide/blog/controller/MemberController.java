b825e81a966270fd03de070eb8f6cde7570ec9b4
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-02 00:22:02 +0900

    수정 Controller

--- src/main/java/com/guide/blog/controller/MemberController.java
+++ src/main/java/com/guide/blog/controller/MemberController.java
@@ -2,15 +2,8 @@ package com.guide.blog.controller;
 
 import com.guide.blog.domain.Member;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
 
-    @RequestMapping("/member/insert")
-    public String insert(Model model) {
-        model.addAttribute("member", new Member());
-        return "member/make";
-    }
 }
