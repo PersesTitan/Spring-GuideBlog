ac2d496f4007fe8eb4a0b6f8282d75e5a28bfd8e
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-02 00:21:34 +0900

    수정 Member

--- src/main/java/com/guide/blog/domain/Member.java
+++ src/main/java/com/guide/blog/domain/Member.java
@@ -1,23 +1,37 @@
 package com.guide.blog.domain;
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
- * id - 해당 전용 아이디 <br>
+ * <p>
+ * id - 해당 전용 아이디
+ * PK 정의, AUTO 정의 <br>
+ * </p>
+ *
  * name - 아이디 이름 <br>
  * password - 비밀번호 <br>
  * date - 아이디를 생성한 날짜 <br>
- * email - 이메일
+ * email - 이메일 <br>
  */
 @Data
+@RequiredArgsConstructor
 public class Member {
-    @NotNull long id;
-    @NotBlank String name;
-    @NotBlank String password;
-    @NotBlank Date date;
-    String email;
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull @NotBlank
+    private String name;
+    @NonNull @NotBlank
+    private String password;
+
+    @NotBlank
+    private Date date;
+    private String email;
 }
