276acabdc3f4c40bcd5e7710c3989366aafb9474
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-06 18:55:13 +0900

    테스트 수정

--- src/test/java/com/guide/blog/MemberRepositoryTest.java
+++ src/test/java/com/guide/blog/MemberRepositoryTest.java
@@ -9,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +23,16 @@ public class MemberRepositoryTest {
     @Rollback(value = false)
     void repositoryTest() throws Exception {
         //데이터 여러개 넣기
+        String password = "1234";
         String[] names = {"TESTER1", "TESTER2", "TESTER3"};
         new ArrayList<>(List.of(names))
                 .forEach(view -> {
                     //given
-                    Member member = new Member(view, view, "1234", null);
+                    Member member = new Member(view, view, password, null);
                     //when
-                    Long saveId = memberRepository.save(member);
-                    Member findMember = memberRepository.find(saveId);
+                    memberRepository.save(member);
+                    Long id = memberRepository.findId(view, password);
+                    Member findMember = memberRepository.findOne(id);
                     //then
                     Assertions.assertThat(member).isEqualTo(findMember);
                     System.out.println(member);
