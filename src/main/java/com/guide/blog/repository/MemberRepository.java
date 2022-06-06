cd7cc6501314fb65fa5eeee95467b8e1e2f6280f
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-06 18:54:07 +0900

    Repository에 id, 맴버를 구하는 메소드 추가

--- src/main/java/com/guide/blog/repository/MemberRepository.java
+++ src/main/java/com/guide/blog/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -17,9 +18,16 @@ public class MemberRepository {
      * @param member 정보 가져옴
      * @return 아이디 반환
      */
-    public Long save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
+    }
+
+    public Long findId(String loginId, String password) {
+        return em.createQuery("SELECT M FROM Member M WHERE M.loginId = :loginId AND M.password = :password", Member.class)
+                .setParameter("loginId", loginId)
+                .setParameter("password", password)
+                .getSingleResult()
+                .getId();
     }
 
     /**
@@ -27,8 +35,19 @@ public class MemberRepository {
      * @param id 아이디 받아오기
      * @return 조회된 아이디 반환
      */
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("SELECT Member FROM Member", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(Long id) {
+        return em.createQuery("SELECT M FROM Member AS M WHERE M.id = :id", Member.class)
+                .setParameter("id", id)
+                .getResultList();
     }
 
 }
